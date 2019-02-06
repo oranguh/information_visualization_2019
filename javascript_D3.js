a504571d2c3ec6dc52855d933f2098f060cd8f63
marco <makoheuvelman@hotmail.com>
2019-02-06 18:05:16 +0100

    meh

--- javascript_D3.js
+++ javascript_D3.js
@@ -1,7 +1,21 @@
+function reqListener(){
 
+  // var jsondata = JSON.parse(this.responseText);
+  $(document).ready(function() {
+     document.write("Hello, World!");
+     console.log("hello3");
+  });
 
+  console.log("hello2");
+  // jQuery.get('meteo.csv', function(data) {
+  //    alert(data);
+  //    //process text file line by line
+  //    $('#body').html(data.replace('n',''));
+  //    console.log("hello");
+  //    console.log(data);
+  // });
 
-
+  }
 
 
 
@@ -22,8 +36,8 @@
 
 
 var requester = new XMLHttpRequest();
-var gitthingy = "https://oranguh.github.io/dataprocessing/Homework/Week_3/enron.json";
-// var gitthingy = "enron.json"
+var gitthingy = "https://oranguh.github.io/information_visualization_2019/meteo.json";
+// var gitthingy = "meteo.json"
 requester.addEventListener("load", reqListener);
 requester.open("GET", gitthingy);
 requester.send();
